70b2d5cd2004e61fb5f3071df9caf675682e9e71
minimAluminiumalism <25639317+minimAluminiumalism@users.noreply.github.com>
2022-04-08 23:44:43 +0800

    update: bug fixed

--- report.py
+++ report.py
@@ -9,11 +9,13 @@ import sys
 import argparse
 from bs4 import BeautifulSoup
 
+
 class Report(object):
     def __init__(self, stuid, password, data_path):
         self.stuid = stuid
         self.password = password
         self.data_path = data_path
+        self.login_page_url = 'https://passport.ustc.edu.cn/login?service=https%3A%2F%2Fweixine.ustc.edu.cn%2F2020%2Fcaslogin'
 
     def report(self):
         loginsuccess = False
@@ -28,17 +30,17 @@ class Report(object):
             else:
                 print("Login Successful!")
                 loginsuccess = True
+        print(loginsuccess)
         if not loginsuccess:
             return False
         data = getform.text
-        data = data.encode('ascii','ignore').decode('utf-8','ignore')
+        data = data.encode('ascii', 'ignore').decode('utf-8', 'ignore')
         soup = BeautifulSoup(data, 'html.parser')
         token = soup.find("input", {"name": "_token"})['value']
-
         with open(self.data_path, "r+") as f:
             data = f.read()
             data = json.loads(data)
-            data["_token"]=token
+            data["_token"] = token
 
         headers = {
             'authority': 'weixine.ustc.edu.cn',
@@ -50,44 +52,42 @@ class Report(object):
             'referer': 'http://weixine.ustc.edu.cn/2020/',
             'accept-language': 'zh-CN,zh;q=0.9',
             'Connection': 'close',
-            'cookie': 'PHPSESSID=' + cookies.get("PHPSESSID") + ";XSRF-TOKEN=" + cookies.get("XSRF-TOKEN") + ";laravel_session="+cookies.get("laravel_session"),
+            'cookie': 'PHPSESSID=' + cookies.get("PHPSESSID") + ";XSRF-TOKEN=" + cookies.get(
+                "XSRF-TOKEN") + ";laravel_session=" + cookies.get("laravel_session"),
         }
-
-        url = "http://weixine.ustc.edu.cn/2020/daliy_report"
-        session.post(url, data=data, headers=headers)
-        data = session.get("http://weixine.ustc.edu.cn/2020").text
-        soup = BeautifulSoup(data, 'html.parser')
-        pattern = re.compile("2020-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}")
-        token = soup.find(
-            "span", {"style": "position: relative; top: 5px; color: #666;"})
-        flag = False
-        if pattern.search(token.text) is not None:
-            date = pattern.search(token.text).group()
-            print("Latest report: " + date)
-            date = date + " +0800"
-            reporttime = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S %z")
-            timenow = datetime.datetime.now(pytz.timezone('Asia/Shanghai'))
-            delta = timenow - reporttime
-            print("{} second(s) before.".format(delta.seconds))
-            if delta.seconds < 120:
-                flag = True
-        if flag == False:
-            print("Report FAILED!")
-        else:
-            print("Report SUCCESSFUL!")
-        return flag
+        url = "https://weixine.ustc.edu.cn/2020/daliy_report"
+        response = session.post(url, data=data, headers=headers)
+        if response.status_code == 200 and "上报成功" in response.text:
+            print("Daily report successfully.")
+            return True
+        return False
 
     def login(self):
-        url = "https://passport.ustc.edu.cn/login?service=http%3A%2F%2Fweixine.ustc.edu.cn%2F2020%2Fcaslogin"
+        r = requests.get(self.login_page_url)
+        init_cookie = r.headers['Set-Cookie'].split(';')[0]
+        soup = BeautifulSoup(r.text, 'lxml')
+        cas_lt = soup.find('input', id="CAS_LT")['value']
+
+        headers = {
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+            'Referer': 'https://passport.ustc.edu.cn/login?service=https%3A%2F%2Fweixine.ustc.edu.cn%2F2020%2Fcaslogin',
+            'Cookie': '{}; lang=zh'.format(init_cookie),
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
         data = {
             'model': 'uplogin.jsp',
-            'service': 'http://weixine.ustc.edu.cn/2020/caslogin',
+            'CAS_LT': cas_lt,
+            'service': 'https://weixine.ustc.edu.cn/2020/caslogin',
             'username': self.stuid,
-            'password': str(self.password),
+            'password': self.password,
+            'warn': '',
+            'showCode': '',
+            'button': ''
         }
         session = requests.Session()
-        session.post(url, data=data)
-
+        login_url = "https://passport.ustc.edu.cn/login"
+        r = session.post(login_url, data=data, headers=headers)
         print("login...")
         return session
 
